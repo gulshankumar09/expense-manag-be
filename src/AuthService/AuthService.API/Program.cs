f64f50ab7af55b1d036a8b299c0313632c53afe8
Gulshan <gk326747@gmail.com>
2025-02-08 17:16:17 +0530

    update

--- src/AuthService/AuthService.API/Program.cs
+++ src/AuthService/AuthService.API/Program.cs
@@ -40,8 +40,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    //app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 // Add authentication & authorization
 app.UseAuthentication();
